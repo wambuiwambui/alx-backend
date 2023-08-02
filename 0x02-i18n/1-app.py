6fcee329315ed512570f1f7aedbca103a6d19da7
wambuiwambui <tnjoki11@gmail.com>
2023-08-02 13:09:25 +0300

    done

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Babel setup"""
 
+
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 app = Flask(__name__)
 babel = Babel(app)
-
 
 class Config:
     """Babel class config"""
@@ -22,6 +23,6 @@ def index():
     return render_template('1-index.html')
 
 
-if __name__ == '__main__':
+if __name__=='__main__':
     app.run(debug=True, port=5001)
 
