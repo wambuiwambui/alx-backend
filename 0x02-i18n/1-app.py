1a4ae13a11ad952a130afdb75cb5499ee69c8415
wambuiwambui <tnjoki11@gmail.com>
2023-08-02 13:16:26 +0300

    done

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 """Babel setup"""
 
-
 from flask import Flask, render_template
 from flask_babel import Babel
-
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -14,6 +12,7 @@ class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 app.config.from_object(Config)
 
 
@@ -22,7 +21,7 @@ def index():
     """Render html doc"""
     return render_template('1-index.html')
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True, port=5001)
+
 
